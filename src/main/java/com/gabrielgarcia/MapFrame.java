b39372d6798b2899f236b475e46ebde788987800
gabrielgarcia-01 <122548028+gabrielgarcia-01@users.noreply.github.com>
2023-04-26 15:24:12 -0400

    still broken

--- src/main/java/com/gabrielgarcia/MapFrame.java
+++ src/main/java/com/gabrielgarcia/MapFrame.java
@@ -1,26 +1,2 @@
-package com.gabrielgarcia;
-
-import javax.swing.*;
-
-public class MapFrame extends JFrame {
-    TileMap tileMap;
-    ImageIcon icon = new ImageIcon("compass.png");
-    public MapFrame() {
-       tileMap = new TileMap();
-       this.setResizable(false);
-       this.setTitle("Map Generator");
-       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       this.setIconImage(icon.getImage());
-       this.add(tileMap);
-       this.setVisible(true);
-    }
-    public MapFrame(long seed, Colors colors, int size) {
-        tileMap = new TileMap(seed, colors, size);
-        this.setResizable(false);
-        this.setTitle("Map Generator");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setIconImage(icon.getImage());
-        this.add(tileMap);
-        this.setVisible(true);
-    }
+package com.gabrielgarcia;public class MapFrame {
 }
